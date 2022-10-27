030d3223573422b1deac44d5ab22003f4ece056d
Teun0n <simon.a.andreasen@gmail.com>
2022-10-27 16:08:54 +0200

    1

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,9 +1,52 @@
 #include "insertion_sort.h"
 #include "linked_list.h"
+#include <stdio.h>
 
 
 
 void sort(linked_list *llPtr)
 {
  // Add your sort function here
+
+node_t *curr=llPtr->head->next;
+node_t *last_sort = llPtr->head;
+node_t *ptr_first=createNode(0);
+ptr_first->next=llPtr->head;
+
+    while(curr!=NULL)
+    {
+        ptr_first->next=llPtr->head;
+        //curr->next=last_sort->next;
+
+        if(curr->data<last_sort->data)
+        {
+            last_sort->next=curr->next;
+
+            if (curr->data<llPtr->head->data)
+            {
+                curr->next=llPtr->head;
+                llPtr->head=curr;
+            }
+            else
+            {
+                while(ptr_first->next->data<curr->data)
+                {
+                    if(ptr_first->next->next->data>=curr->data)
+                    {
+                        curr->next=ptr_first->next->next;
+                        ptr_first->next->next=curr;
+                    }
+                    ptr_first=ptr_first->next;
+                }
+            }
+            curr=last_sort->next;
+
+        }
+        else
+        {
+        curr=curr->next;
+        last_sort=last_sort->next;
+        }
+        
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 extern "C"{
     // Add the header files required to run your main 
+    #include "insertion_sort.c"
 }
-
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
 {
