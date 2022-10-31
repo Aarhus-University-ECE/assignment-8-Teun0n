1583a58f321e6fa2323899c7b97834abc7590dec
Teun0n <simon.a.andreasen@gmail.com>
2022-10-31 21:07:09 +0100

    text added

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -7,11 +7,12 @@
 void sort(linked_list *llPtr)
 {
  // Add your sort function here
-/*
-node_t *curr=llPtr->head->next;//points to value being checked
-node_t *last_sort = llPtr->head;
-node_t *ptr_first=createNode(0);
-ptr_first->next=llPtr->head;
+//pointing to NULL at some point, should run if fixed
+
+node_t *curr=llPtr->head->next;//points to value being checked, starts at second node.
+node_t *last_sort = llPtr->head;//points to last sorted node, starts at first node
+node_t *ptr_first=createNode(0); //"dummy node" which has a meaningless value.
+ptr_first->next=llPtr->head; //"dummy node" is set to point to first node.
 
     while(curr!=NULL)
     {
@@ -19,27 +20,34 @@ ptr_first->next=llPtr->head;
         ptr_first->next=llPtr->head;//is reset to point to first element
         
 
-        if(curr->data<last_sort->data) //checks if 
+        if(curr->data<last_sort->data) //checks if next node is larger than last sorted
         {
-            last_sort->next=curr->next;
+            last_sort->next=curr->next; // if next node is larger then the order is correct
 
-            if (curr->data<llPtr->head->data)
+            if (curr->data<llPtr->head->data) // if current node is smaller than first node
             {
-                curr->next=llPtr->head;
-                llPtr->head=curr;
+                curr->next=llPtr->head; //current node's next is set to first node
+                llPtr->head=curr; //and llPtr head, which is the first value in node_t, is set to current value.
             }
-            else
+            else //if current data isn't smaller than next first node:
             {
+                //while(ptr_first->next->data<=curr->data)
                 while(ptr_first->next->data<curr->data)
+                
                 {
+                    
                     printf("while2 \n");
-                    if(ptr_first->next->next->data>=curr->data)
+                    if(ptr_first->next->next->data>=curr->data)//runs through nodes starting from 2nd node until 
                     {
+                    
                         curr->next=ptr_first->next->next;
                         ptr_first->next->next=curr;
                     }
-                    ptr_first=ptr_first->next;
-                    break;
+                    if(ptr_first->next->data<curr->data)
+                    {
+                        ptr_first=ptr_first->next;
+                    }
+                    
                 }
             }
             curr=last_sort->next;
@@ -51,5 +59,5 @@ ptr_first->next=llPtr->head;
         last_sort=last_sort->next;//moves up a node.
         }
         
-    }*/
+    }
 }
