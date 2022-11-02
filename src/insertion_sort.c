e114b56497a36c7c01485ca2a6e1a5e36c40ca4a
Teun0n <simon.a.andreasen@gmail.com>
2022-11-02 11:45:59 +0100

    insertion_sort complete

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -8,55 +8,39 @@
 void sort(linked_list *llPtr)
 {
  // Add your sort function here
-//pointing to NULL at some point, should run if fixed
-assert(llPtr->head!=NULL);//if list is empty it can't be sorted.
-node_t *curr=llPtr->head->next;//points to value being checked, starts at second node.
-node_t *last_sort = llPtr->head;//points to last sorted node, starts at first node
-node_t *ptr_first=createNode(0); //"dummy node" which has a meaningless value.
-ptr_first->next=llPtr->head; //"dummy node" is set to point to first node.
+    node_t* curr = llPtr->head->next; //current node
+    node_t* last_sort = llPtr->head;//last sorted node
+    node_t* temp;
 
-    while(curr!=NULL)
+    while(curr != NULL)
     {
-        printf("while 1 \n");
-        ptr_first->next=llPtr->head;//is reset to point to first element
-        
-
-        if(curr->data<last_sort->data) //checks if next node is larger than last sorted
+        if (curr->data < last_sort->data) // if currents data is less than last sorted data.
         {
-            last_sort->next=curr->next; // if next node is larger then the order is correct
-
-            if (curr->data<llPtr->head->data) // if current node is smaller than first node
-            {
-                curr->next=llPtr->head; //current node's next is set to first node
-                llPtr->head=curr; //and llPtr head, which is the first value in node_t, is set to current value.
-            }
-            else //if current data isn't smaller than next ptr_first:
-            {
-                //while(ptr_first->next->data<=curr->data)
-                while(ptr_first->next->data>curr->data)
-                
-                {
-                    printf("while2 \n");
-                    if(ptr_first->next->data<=curr->data)
-                    {
-                        curr->next=ptr_first->next->next;
-                        break;
-                    }
-                    else
-                    {
-                        ptr_first=ptr_first->next;
-                    }
+            last_sort->next=curr->next; //last_sorts next needs to be point at curr next, before currs next is changed
+           
+            temp = llPtr->head;//temp is set to point at first node.
+             while (temp->next->data <= curr->data)
+             /*temp is moved up the nodes until curr is larger than temps next,
+              which is its right spot.*/
+             {
+                  temp = temp->next;//temp is moved to next node.
                 }
-                 
-            }
-            last_sort=curr;
+
+              
+               if (llPtr->head==temp)//if first value is equal to temp at any point.
+               {
+                    curr->next=llPtr->head; // curr next is set to point at first value
+                     llPtr->head = curr; //first value is redefined to curr 
+
+               }
+               else 
+               {
+            
+                curr->next = temp->next;//curr is set to point at temps next
+                temp->next = curr; // temp is set to point at curr.
+                }
         }
-        else// if curr is larger than last sort
-        {
-        curr=curr->next;//moves up a node.
-        last_sort=last_sort->next;//moves up a node.
-        }
-        
+        last_sort = curr; //moves last_sorted to next node.
+        curr = last_sort->next;//moves curr to next node.
     }
-    
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@ extern "C"{
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
 {
-    
+    int randomArray[7] ={-3,22,11,33,3,2,1};
+     linked_list * llPtr = convertArrayToLinkedList(randomArray,7);
+     for(int i=0; i<7,i++)
+     {
+        printf("%d  ",linked_list)
+     }
     return 0;
 }
