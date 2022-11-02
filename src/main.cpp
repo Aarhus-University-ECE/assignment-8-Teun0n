769cc6889ca4174da6a819cb48ce5a0f4b19b823
Teun0n <simon.a.andreasen@gmail.com>
2022-11-02 11:57:00 +0100

    main won't run anything.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 extern "C"{
-    #include "list_queue.c"
     // Add the header files required to run your main 
-    #include "insertion_sort.c"
+    #include "list_queue.c"
+    #include "linked_list.h"
+    #include "list_queue.h"
 }
+#include <assert.h>
+
+
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
 {
-    int randomArray[7] ={-3,22,11,33,3,2,1};
-     linked_list * llPtr = convertArrayToLinkedList(randomArray,7);
-     for(int i=0; i<7,i++)
-     {
-        printf("%d  ",linked_list)
-     }
+
     return 0;
 }
